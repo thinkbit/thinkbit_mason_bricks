98827306c200f56f9678d4ec1b022a3b28e20826
Ralph Francis Pon-an <ponan.ralph@yahoo.com>
2024-08-21 23:54:57 +0800

    feat: auto_route router, auth_guard, app_route_observer

--- bricks/auto_route/hooks/post_gen.dart
+++ bricks/auto_route/hooks/post_gen.dart
@@ -5,8 +5,7 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing Packages');
 
-  await Process.run('bash', ['-c', 'flutter pub add dio']);
-  await Process.run('bash', ['-c', 'flutter pub add retry']);
+  await Process.run('bash', ['-c', 'flutter pub add auto_route']);
   await Process.run('bash', ['-c', 'flutter pub get']);
 
   progress.complete();
