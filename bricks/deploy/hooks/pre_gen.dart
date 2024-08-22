bcd60b3c0f093448692096642608742e261eed25
Makoy <makoy@thinkbitsolutions.com>
2024-08-22 16:10:51 +0800

    chore: updated pre_gen.dart

--- bricks/deploy/hooks/pre_gen.dart
+++ bricks/deploy/hooks/pre_gen.dart
@@ -1,5 +1,22 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add pre-generation logic.
+Future<void> run(HookContext context) async {
+  final progress = context.logger.progress('Installing Packages');
+
+  await Process.run(
+    'flutter',
+    ['pub', 'add', 'dio', 'retry'],
+    runInShell: true,
+  );
+
+  await Process.run(
+    'flutter',
+    ['pub', 'get'],
+    runInShell: true,
+  );
+
+  progress.complete();
+  context.logger.success('Done instaling packages!');
 }
