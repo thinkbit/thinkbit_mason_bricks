4fdaf1dcfbb06dba8213769ba25b638e5143a36e
Makoy <makoy@thinkbitsolutions.com>
2026-03-11 16:16:57 +0800

    feat(validator): polish validator brick, fix typos and logic errors

--- bricks/validator/__brick__/lib/utils/validator/validator_constant.dart
+++ bricks/validator/__brick__/lib/utils/validator/validator_constant.dart
@@ -6,11 +6,6 @@ const emailRegex =
 const phoneRegex = r'^[\d,;*+#\- .()/N]+$';
 const passwordRegex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$';
 
-///  Integers
-const defaultTwo = 2;
-const defaultOne = 1;
-const defaultZero = 0;
-
 /// Requirement Messages
 const emailRequirementMessage = 'This must be an email address.';
 const integerRequirementMessage = 'This field must be an integer.';
@@ -18,5 +13,5 @@ const numericRequirementMessage = 'This field must be a number.';
 const passwordRequirementMessage =
     'Must contain at least one uppercase & lowercase letter, number\nand special characters.';
 const requiredMessage = 'This field is required.';
-const regExInvalid = 'This format is valid.';
-const phoneRequirementMesage = 'This must be a valid phone number.';
+const regExInvalid = 'This format is invalid.';
+const phoneRequirementMessage = 'This must be a valid phone number.';
